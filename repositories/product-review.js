b8757475b20ed6747accf1dc4f2b783c0d808321
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-05-13 15:33:19 -0500

    add zod on post review

--- repositories/product-review.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductReviewRepository = void 0;
-const product_review_1 = require("../models/product-review");
-const database_1 = require("@medusajs/medusa/dist/loaders/database");
-exports.ProductReviewRepository = database_1.dataSource.getRepository(product_review_1.ProductReview);
-//# sourceMappingURL=product-review.js.map
