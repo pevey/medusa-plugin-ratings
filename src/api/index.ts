af34d1dd5efbe53c5baeeec0cbc1e4561986571d
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-05-14 09:31:43 -0500

    add back cors, authenticate still not working

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,19 +1,25 @@
+import cors from "cors"
+import configLoader from "@medusajs/medusa/dist/loaders/config"
 import { Router } from "express"
 import * as bodyParser from "body-parser"
 //import { User, UserService, Customer, CustomerService } from "@medusajs/medusa"
-//import authenticate from "@medusajs/medusa/dist/api/middlewares/authenticate"
+import { authenticateCustomer } from "@medusajs/medusa"
 import { MedusaError } from "medusa-core-utils"
 import { z } from "zod"
 
-export default (): Router | Router[] => {
+export default (rootDirectory: string): Router | Router[] => {
+
+	const config = configLoader(rootDirectory)
+	const storeCorsOptions = { origin: config.projectConfig.store_cors.split(","), credentials: true, }
+   const adminCorsOptions = { origin: config.projectConfig.admin_cors.split(","), credentials: true, }
 
    const router = Router()
 
    // REVIEWS - GET ALL REVIEWS FOR A PRODUCT
-//router.use("/store/products/:id/reviews", authenticate(), getCustomer)
-	router.get("/store/products/:id/reviews", async (req, res) => {
-// console.log('get reviews')
-// console.log(req.customer)
+	//router.use("/store/products/:id/reviews", authenticateCustomer())
+	router.get("/store/products/:id/reviews", cors(storeCorsOptions), authenticateCustomer(), async (req, res) => {
+console.log('get reviews')
+console.log(req.user)
       const productReviewService = req.scope.resolve("productReviewService")
       productReviewService.getProductReviews(req.params.id).then((product_reviews) => {
          return res.json({product_reviews})
@@ -33,7 +39,6 @@ export default (): Router | Router[] => {
    //router.use("/store/products/:id/reviews", authenticate(), getCustomer, bodyParser.json())
 	router.use("/store/products/:id/reviews", bodyParser.json())
    router.post("/store/products/:id/reviews", async (req, res) => {
-      const productReviewService = req.scope.resolve("productReviewService")
 		const schema = z.object({
 			customer_id: z.string().min(1),
 			display_name: z.string().min(1),
@@ -45,6 +50,7 @@ export default (): Router | Router[] => {
 		if (!success) {
 			throw new MedusaError(MedusaError.Types.INVALID_DATA, error)
 		} else {
+			const productReviewService = req.scope.resolve("productReviewService")
 			productReviewService.addProductReview(req.params.id, data.customer_id, data.display_name, data.content, data.rating)
 			.then((product_review) => {
 				return res.json({product_review})
@@ -53,45 +59,58 @@ export default (): Router | Router[] => {
    })
 
    // REVIEWS - GET A SINGLE REVIEW
-   // router.get("/store/reviews/:id", async (req, res) => {
-   //    const productReviewService = req.scope.resolve("productReviewService")
-   //    productReviewService.getReview(req.params.id).then((product_review) => {
-   //       return res.json({product_review})
-   //    })
-   // })
+   router.get("/store/reviews/:id", async (req, res) => {
+      const productReviewService = req.scope.resolve("productReviewService")
+      productReviewService.getReview(req.params.id).then((product_review) => {
+         return res.json({product_review})
+      })
+   })
 
    // REVIEWS - UPDATE A REVIEW FOR A PRODUCT
    //router.use("/store/reviews/:id", authenticate(), getCustomer, bodyParser.json())
-   // router.post("/store/reviews/:id", async (req, res) => {
-
-   //    const productReviewService = req.scope.resolve("productReviewService")
-   //    const productReview = await productReviewService.getProductReviews(req.params.id)
-   //    if (productReview.customer_id !== req.body.userId) {
-   //       return res.status(401).json({ message: "Unauthorized" })
-   //    }
-   //    productReviewService.updateProductReview(req.params.id, req.body.display_name, req.body.content, req.body.rating)
-   //    .then((product_review) => {
-   //       return res.json({product_review})
-   //    })
-   // })
+	router.use("/store/reviews/:id", bodyParser.json())
+   router.post("/store/reviews/:id", async (req, res) => {
+		const schema = z.object({
+			customer_id: z.string().min(1),
+			display_name: z.string().min(1),
+			content: z.string().min(1),
+			rating: z.coerce.number().min(0).max(5),
+		})
+		/* @ts-ignore */
+		const { success, error, data } = schema.safeParse(req.body)
+		if (!success) {
+			throw new MedusaError(MedusaError.Types.INVALID_DATA, error)
+		} else {
+		}
+      const productReviewService = req.scope.resolve("productReviewService")
+      const productReview = await productReviewService.getProductReviews(req.params.id)
+      if (productReview.customer_id !== req.body.userId) {
+         return res.status(401).json({ message: "Unauthorized" })
+      }
+      productReviewService.updateProductReview(req.params.id, req.body.display_name, req.body.content, req.body.rating)
+      .then((product_review) => {
+         return res.json({product_review})
+      })
+   })
 
    // REVIEWS - ADMIN GET ALL REVIEWS FOR A PRODUCT
-   // router.get("/admin/products/:id/reviews", async (req, res) => {
-   //    const productReviewService = req.scope.resolve("productReviewService")
-   //    productReviewService.getProductReviews(req.params.id).then((product_reviews) => {
-   //       return res.json({product_reviews})
-   //    })
-   // })
+   router.get("/admin/products/:id/reviews", async (req, res) => {
+      const productReviewService = req.scope.resolve("productReviewService")
+      productReviewService.getProductReviews(req.params.id).then((product_reviews) => {
+         return res.json({product_reviews})
+      })
+   })
 
    // REVIEWS - ADMIN EDIT A REVIEW FOR A PRODUCT
    //router.use("/admin/reviews/:id", bodyParser.json(), authenticate(), getUser)
-   // router.post("/admin/reviews/:id", async (req, res) => {
-   //    const productReviewService = req.scope.resolve("productReviewService")
-   //    productReviewService.editProductReview(req.params.id, req.body.display_name, req.body.content, req.body.rating, req.body.approved)
-   //    .then((product_review) => {
-   //       return res.json({product_review})
-   //    })
-   // })
+	router.use("/admin/reviews/:id", bodyParser.json())
+   router.post("/admin/reviews/:id", async (req, res) => {
+      const productReviewService = req.scope.resolve("productReviewService")
+      productReviewService.editProductReview(req.params.id, req.body.display_name, req.body.content, req.body.rating, req.body.approved)
+      .then((product_review) => {
+         return res.json({product_review})
+      })
+   })
 
    return router
 }
